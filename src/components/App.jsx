10ba6e9ef28fe90423e65b4ce5939f96859f38fd
Ginger Williams <gingerwilliams@MacBook-Pro.local>
2018-10-31 12:39:00 -0400

    applied DS header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return (
-            <div>this is my app</div>
+            <div>
+                                                
+                <div className="header-base_wrap">
+                    <header className="header-base header-base_box">
+                        <a className="logo logo_box logo_newell-brands-base"></a>
+                        <div className="header-base_title" alt="Website Title">Website Title</div>
+                    </header>
+                </div>
+                
+                <div className="text-align__center">this is my app</div>
+            </div>
         )
     }
 }
