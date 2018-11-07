bcec423f6c10bc40d57718511cf3c41cc312537f
Ginger Williams <gingerwilliams@MacBook-Pro.local>
2018-11-07 09:06:28 -0500

    title code clean up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ class App extends React.Component {
                 <div className="header-base_wrap">
                     <header className="header-base header-base_box">
                         <a className="logo logo_box logo_newell-brands-base"></a>
-                        <div className="header-base_title" alt="Website Title">Website Title</div>
+                        <div className="header-base_title" alt="Website Title">React Shrinkwrap and Login</div>
                     </header>
                 </div>
             
